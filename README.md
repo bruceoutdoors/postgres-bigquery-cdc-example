1707230591a64819fdd58fc0ab4a255794bd8768
Lee Zhen Yong <bruceoutdoors@gmail.com>
2020-05-31 11:23:19 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Postgres to BigQuery CDC Pipeline Example
+=========================================
+
+A CDC pipeline that streams postgres database table changes to Bigquery via Debezium, Avro, Dataflow+Python.
+
+TODO: the dataflow part
+
+## Quickstart
+
+```sh
+# Start debezium + example postgres db
+docker-compose up
+
+# Setup connector
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @register-postgres.json
+
+# Access postgres database
+psql postgresql://postgres:postgres@localhost:5432/postgres
+
+# Start test kafka client
+pip3 install -r requirements.txt
+python3 kafka-client.py
+```
---
+++ kafka-client.py
@@ -0,0 +1,28 @@
+from confluent_kafka import DeserializingConsumer
+from simple_avro_deserializer import SimpleAvroDeserializer
+
+if __name__ == '__main__':
+    brucedeserializer = SimpleAvroDeserializer('http://127.0.0.1:8081')
+    consumer_conf = {'bootstrap.servers' : 'localhost:9092',
+                     'value.deserializer': brucedeserializer,
+                     'key.deserializer'  : brucedeserializer,
+                     'group.id'          : 'mygroup',
+                     'auto.offset.reset' : "earliest"}
+
+    consumer = DeserializingConsumer(consumer_conf)
+    consumer.subscribe(['dbserver1.inventory.customers'])
+
+    while True:
+        try:
+            # SIGINT can't be handled when polling, limit timeout to 1 second.
+            msg = consumer.poll(1.0)
+            if msg is None:
+                continue
+
+            data = msg.value()
+            if data is not None:
+                print(data)
+        except KeyboardInterrupt:
+            break
+
+    consumer.close()
