79544688f2df5caab932a1035c598d4a46bedcb4
Lee Zhen Yong <bruceoutdoors@gmail.com>
2020-06-13 21:54:09 +0800

    theoratically working code

--- src/main/java/bruceoutdoors/beam/examples/PostgresCDCBigQuery.kt
+++ src/main/java/bruceoutdoors/beam/examples/PostgresCDCBigQuery.kt
@@ -17,16 +17,22 @@
  */
 package bruceoutdoors.beam.examples
 
+import avro.shaded.com.google.common.collect.ImmutableList
 import avro.shaded.com.google.common.collect.ImmutableMap
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import org.apache.avro.generic.GenericRecord
 import org.apache.beam.sdk.Pipeline
+import org.apache.beam.sdk.coders.AvroCoder
 import org.apache.beam.sdk.io.kafka.KafkaIO
 import org.apache.beam.sdk.options.*
 import org.apache.beam.sdk.transforms.*
 import org.apache.beam.sdk.transforms.windowing.FixedWindows
 import org.apache.beam.sdk.transforms.windowing.Window
 import org.apache.beam.sdk.values.KV
+import org.apache.kafka.common.serialization.Deserializer
 import org.joda.time.Duration
 import java.io.IOException
+
 
 object PostgresCDCBigQuery {
     const val WINDOW_SIZE: Long = 2
@@ -41,9 +47,16 @@ object PostgresCDCBigQuery {
         var output: String
     }
 
-    class AvroToRow : InferableFunction<KV<ByteArray, ByteArray>, List<String>>() {
-        override fun apply(line : KV<ByteArray, ByteArray>) : List<String> {
-            return line.key.toString().split("jkd").toList()
+    class AvroToRow : InferableFunction<KV<ByteArray, GenericRecord>, TableRow>() {
+        override fun apply(record: KV<ByteArray, GenericRecord>): TableRow {
+            return TableRow()
+                    .set("id", record.value.get("id") as Long)
+                    .set("first_name", record.value.get("first_name") as String)
+                    .set("last_time", record.value.get("last_time") as String)
+                    .set("email", record.value.get("email") as String)
+                    .set("__op", record.value.get("__op") as String)
+                    .set("__source_ts_ms", record.value.get("__source_ts_ms") as Long)
+                    .set("__lsn", record.value.get("__lsn") as Long)
         }
     }
 
@@ -53,16 +66,54 @@ object PostgresCDCBigQuery {
         val output = options.output
         val p = Pipeline.create(options)
 
+        val tableSpec: TableReference = TableReference()
+                .setProjectId("crafty-apex-264713")
+                .setDatasetId("inventory")
+                .setTableId("customers")
+
+        val tableSchema: TableSchema = TableSchema().setFields(ImmutableList.of(
+                TableFieldSchema()
+                        .setName("id")
+                        .setType("INT64"),
+                TableFieldSchema()
+                        .setName("first_name")
+                        .setType("STRING"),
+                TableFieldSchema()
+                        .setName("last_time")
+                        .setType("STRING"),
+                TableFieldSchema()
+                        .setName("email")
+                        .setType("STRING"),
+                TableFieldSchema()
+                        .setName("__op")
+                        .setType("STRING"),
+                TableFieldSchema()
+                        .setName("__source_ts_ms")
+                        .setType("INT64"),
+                TableFieldSchema()
+                        .setName("__lsn")
+                        .setType("INT64")
+        ))
+
+
         p.apply("Read from Kafka",
-                KafkaIO.read<ByteArray, ByteArray>()
+                KafkaIO.read<ByteArray, GenericRecord>()
                         .withBootstrapServers("localhost:9092")
                         .withTopic("dbserver1.inventory.customers")
-                        .updateConsumerProperties(ImmutableMap.of("auto.offset.reset", "earliest") as Map<String, Any>?)
+                        .withConsumerConfigUpdates(ImmutableMap.of("auto.offset.reset", "earliest" as Any))
+                        .withConsumerConfigUpdates(ImmutableMap.of("specific.avro.reader", "true" as Any))
+                        .withValueDeserializerAndCoder(KafkaAvroDeserializer::class.java as Class<out Deserializer<GenericRecord>>, AvroCoder.of(GenericRecord::class.java))
                         .withoutMetadata()
         ).apply("2 Second Window",
-                Window.into<KV<ByteArray, ByteArray>>(FixedWindows.of(Duration.standardSeconds(WINDOW_SIZE)))
+                Window.into<KV<ByteArray, GenericRecord>>(FixedWindows.of(Duration.standardSeconds(WINDOW_SIZE)))
         ).apply("Avro to Row",
-                FlatMapElements.via(AvroToRow())
+                MapElements.via(AvroToRow())
+        ).apply("Write to BigQuery",
+                BigQueryIO.writeTableRows()
+                        .to(tableSpec)
+                        .withSchema(tableSchema)
+                        .withCreateDisposition(CreateDisposition.CREATE_IF_NEEDED)
+                        .withWriteDisposition(WriteDisposition.WRITE_TRUNCATE)
         )
 
         p.run().waitUntilFinish()
